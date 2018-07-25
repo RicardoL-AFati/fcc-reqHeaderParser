f98199f9ebae2f21366970f4062f82569cac9328
RicardoL-AFati <rickydem24@gmail.com>
2018-07-25 17:18:17 -0600

    added api GET route to parse req

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
+// socket -> Socket.TCP.owner
+app.get('/api/whoami', (req, res) => {
+  res.send({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
+});
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
