05bfb4663176c8c7106e442af1be100b16a1c8a2
RicardoL-AFati <rickydem24@gmail.com>
2018-07-25 17:43:25 -0600

    different ip for req

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const app = express();
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/api/whoami', (req, res) => {
+  console.log(req.headers['x-forwarded-for']);
+  console.log(req.header['x-forwarded-for']);
+  console.log(req.connection.remoteAddress);
   res.send({
-    ipaddress: req.ip,
+    ipaddress: req.connection.remoteAddress,
     language: req.headers['accept-language'],
     software: req.headers['user-agent'],
   });
