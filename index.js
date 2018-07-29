0ff1a344c1eec5962b9fa8808ed4ec26cb8f0832
RicardoL-AFati <rickydem24@gmail.com>
2018-07-28 19:54:43 -0600

    added comments to index

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+// Creating app and adding middleware
 const app = express();
-
 app.use(cors({ optionsSuccessStatus: 200 }));
+app.use(express.static('build'));
 
+/* GET ROUTES */
+
+// GET api route - returns IP, preferred langs, and system info for device
 app.get('/api/whoami', (req, res) => {
+  // taking ip from req body and assigning to ip
   let { ip } = req;
+  // regex to check for ipv6 placeholder '::ffff:' for ipv4 addresses
   const ipv6Regex = /:{2}f{4}:/;
+  // if present - remove from ip
   if (req.ip.match(ipv6Regex)) {
     ip = ip.replace(ipv6Regex, '');
   }
+  // send JSON with: ip, lang. and system info
   res.send({
     ipaddress: ip,
     language: req.headers['accept-language'],
@@ -18,11 +26,11 @@ app.get('/api/whoami', (req, res) => {
   });
 });
 
-app.use(express.static('build'));
-
+// GET for serving static assets
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
+// Port is production port or localhost:5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
